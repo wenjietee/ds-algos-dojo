113a44583905a22ede0af304d0d23b20b887ef68
Wenjie <wenjietj@gmail.com>
2021-07-30 17:37:02 +0800

    add flatten array recursion

---
+++ recursion/flattenArray.js
@@ -0,0 +1,34 @@
+const arr = [1, 2, [3, 4], [5, [6, [7]]], [[[[[8]]]]]];
+
+// from mozilla https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+function flatDeep(arr, d = 1) {
+	return d > 0
+		? arr.reduce(
+				(acc, val) =>
+					acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val),
+				[]
+		  )
+		: arr.slice();
+}
+
+// manual method w/ recursion
+const flatten = (arr) => {
+	// array to hold flattened contents
+	let result = [];
+
+	// iterate the nested array
+	for (let i = 0; i < arr.length; i++) {
+		// check if element is an array
+		if (Array.isArray(arr[i])) {
+			// flatten it by concat-ing the array with result recursively
+			result = result.concat(flatten(arr[i]));
+		} else {
+			// if element not array push into result
+			result.push(arr[i]);
+		}
+	}
+	return result;
+};
+
+console.log(flatten(arr));
+console.log(flatDeep(arr, Infinity));
