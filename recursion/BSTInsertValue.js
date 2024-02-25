7e9189b17260e254aee7c96a1010df56bf73a9cf
Tee Wenjie <wenjietj@gmail.com>
2024-02-25 23:30:39 +0800

    fix typo

--- recursion/BSTInsertValue.js
+++ recursion/BSTInsertValue.js
@@ -1,17 +1,17 @@
 const insertNodeBinarySearchTree = (head, data) => {
-	//base case
-	if (head === null) {
-		head = new Node();
+    //base case
+    if (head === null) {
+        head = new Node();
 
-		head.data = data;
-		return head;
-	}
+        head.data = data;
+        return head;
+    }
 
-	//recursion
-	if (head.data < data) {
-		head.right = insertnNode(head.right.data);
-	} else {
-		head.left = insertnNode(head.left.data);
-	}
-	return head;
+    //recursion
+    if (head.data < data) {
+        head.right = insertNodeBinarySearchTree(head.right.data);
+    } else {
+        head.left = insertNodeBinarySearchTree(head.left.data);
+    }
+    return head;
 };
